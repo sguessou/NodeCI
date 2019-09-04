fa71a5c58272bba4c5ccc0ef4e5e922bc07b5093
Saad Guessous <guessous_saad@hotmail.com>
2019-09-04 12:06:12 +0300

    added travis CI configs

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+        '430996978111-oj4gecrfe6e2ciflgorqi3gq3j38iu70.apps.googleusercontent.com',
+    googleClientSecret: 'b7BU2qHEojqOp3NJ9sr_LKn7',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: '123123123',
+    redisUrl: 'redis://127.0.0.1:6379',
+};
---
+++ tests/header.test.js
@@ -0,0 +1,33 @@
+const Page = require('./helpers/page');
+
+let page;
+
+beforeEach(async () => {
+    page = await Page.build();
+    await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+    await page.close();
+});
+
+test('the header has the correct text', async () => {
+    const text = await page.getContentsOf('a.brand-logo');
+
+    expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+    await page.click('.right a');
+
+    const url = await page.url();
+
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('when signed in, shows logout button', async () => {
+    await page.login();
+
+    const text = await page.getContentsOf('a[href="/api/logout"]');
+    expect(text).toEqual('Logout');
+});
